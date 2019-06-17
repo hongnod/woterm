de02b5d1a5180debec1b74957cfb5ccf72b180d8
wingo.he <wingo.he@qq.com>
2019-06-18 07:56:25 +0800

    fix

--- woterm/qwohostsimplelist.cpp
+++ woterm/qwohostsimplelist.cpp
@@ -1,5 +1,8 @@
 #include "qwohostsimplelist.h"
 #include "ui_qwohostlist.h"
+#include "qwohostlistmodel.h"
+
+#include <QSortFilterProxyModel>
 
 QWoHostSimpleList::QWoHostSimpleList(QWidget *parent)
     : QDialog(parent)
@@ -8,9 +11,34 @@ QWoHostSimpleList::QWoHostSimpleList(QWidget *parent)
     Qt::WindowFlags flags = windowFlags();
     setWindowFlags(flags &~Qt::WindowContextHelpButtonHint);
     ui->setupUi(this);
+
+    m_model = new QWoHostListModel(this);
+    m_proxyModel = new QSortFilterProxyModel(this);
+    m_proxyModel->setSourceModel(m_model);
+    ui->hostList->setModel(m_proxyModel);
+
+    QObject::connect(ui->rxfind, SIGNAL(textChanged(const QString&)), this, SLOT(onEditTextChanged(const QString&)));
+
+    m_model->refreshList();
 }
 
 QWoHostSimpleList::~QWoHostSimpleList()
 {
 
+}
+
+void QWoHostSimpleList::onEditTextChanged(const QString &txt)
+{
+    QStringList sets = txt.split(' ');
+    for(QStringList::iterator iter = sets.begin(); iter != sets.end(); ) {
+        if(*iter == "") {
+            iter = sets.erase(iter);
+        }else{
+            iter++;
+        }
+    }
+
+    QRegExp regex(sets.join(".*"), Qt::CaseInsensitive);
+    regex.setPatternSyntax(QRegExp::RegExp2);
+    m_proxyModel->setFilterRegExp(regex);
 }
--- woterm/qwohostsimplelist.h
+++ woterm/qwohostsimplelist.h
@@ -4,10 +4,14 @@
 #include "qwoglobal.h"
 
 #include <QDialog>
+#include <QPointer>
 
 namespace Ui {
 class QWoHostList;
 }
+
+class QWoHostListModel;
+class QSortFilterProxyModel;
 
 class QWoHostSimpleList : public QDialog
 {
@@ -18,11 +22,13 @@ public:
     ~QWoHostSimpleList();
 
 private slots:
-
+    void onEditTextChanged(const QString& txt);
 private:
     void init();
 private:
     Ui::QWoHostList *ui;
+    QPointer<QWoHostListModel> m_model;
+    QPointer<QSortFilterProxyModel> m_proxyModel;
 };
 
 #endif // QWOHOSTSIMPLELIST_H
