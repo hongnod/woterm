e8d1046dde31c74ba3a5f6f3f3ffb0179256ee3a
wingo.he <wingo.he@qq.com>
2019-06-28 00:45:12 +0800

    define function.

--- test/main.c
+++ test/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <Windows.h>
 
-typedef void (*FunIpcCallBack)(int hdl, char *funName, ...);
-
+typedef void (*FunIpcCallBack)(int hdl, char *funName, char *argv[]);
 typedef int (*FunIpcConnect)(const char* name, FunIpcCallBack cb);
-typedef int (*FunIpcCall)(int hdl, const char* funName, ...);
+typedef int (*FunIpcCall)(int hdl, const char* funName, char *argv[]);
 typedef int (*FunIpcClose)(int hdl);
+typedef int (*FunIpcInit)(void);
 
-
+static FunIpcInit IpcInit;
 static FunIpcConnect IpcConnect;
 static FunIpcCall IpcCall;
 static FunIpcClose IpcClose;
@@ -20,6 +20,7 @@ int main()
         printf("Can Not Find WoIpc.dll");
         return 0;
     }
+    IpcInit = (FunIpcInit)GetProcAddress(hdl, "IpcInit");
     IpcConnect = (FunIpcConnect)GetProcAddress(hdl, "IpcConnect");
     IpcCall = (FunIpcCall)GetProcAddress(hdl, "IpcCall");
     IpcClose = (FunIpcClose)GetProcAddress(hdl, "IpcClose");
