917187f8b0a680cb7fa4cd5be2a6acd741617ee4
heguowen1 <heguowen1@wps.cn>
2019-08-08 09:32:15 +0800

    fix

--- woterm/qwotermmask.cpp
+++ woterm/qwotermmask.cpp
@@ -6,6 +6,12 @@ QWoTermMask::QWoTermMask(QWidget *parent) :
     ui(new Ui::QWoTermMask)
 {
     ui->setupUi(this);
+
+    setAutoFillBackground(true);
+    QPalette pal;
+    pal.setColor(QPalette::Background, qRgba(128, 128, 128, 128));
+    pal.setColor(QPalette::Window, qRgba(128, 128, 128, 128));
+    setPalette(pal);
 }
 
 QWoTermMask::~QWoTermMask()
