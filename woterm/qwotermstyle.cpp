bc891451a20b72ea3dc490f9bead0c0281d3826b
wingo.he <wingo.he@qq.com>
2019-09-07 21:07:01 +0800

    fix

--- woterm/qwotermstyle.cpp
+++ woterm/qwotermstyle.cpp
@@ -1,9 +1,28 @@
 #include "qwotermstyle.h"
 
+#include <QtWidgets>
 #include <QStyleFactory>
 
 QWoTermStyle::QWoTermStyle()
     : QProxyStyle(QStyleFactory::create("Fusion"))
 {
 
+}
+
+void QWoTermStyle::drawControl(QStyle::ControlElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget) const
+{
+    switch (element) {
+    case CE_TabBarTab:
+        {
+            QStyleOptionTab myOption;
+            const QStyleOptionTab *tabOption = qstyleoption_cast<const QStyleOptionTab *>(option);
+            if (tabOption) {
+
+            }
+            QProxyStyle::drawControl(element, tabOption, painter, widget);
+        }
+        break;
+    default:
+        QProxyStyle::drawControl(element, option, painter, widget);
+    }
 }
--- woterm/qwotermstyle.h
+++ woterm/qwotermstyle.h
@@ -8,6 +8,11 @@ class QWoTermStyle : public QProxyStyle
     Q_OBJECT
 public:
     QWoTermStyle();
+
+protected:
+    virtual void drawControl(QStyle::ControlElement element, const QStyleOption *option, QPainter *painter, const QWidget *widget = nullptr) const override;
+private:
+
 };
 
 #endif // QWOTERMSTYLE_H
